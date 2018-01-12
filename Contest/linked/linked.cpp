4c6977d9fca8b2d067854b5b69aae06ad6b95d90
Fungucide <bluestich227@gmail.com>
2018-01-11 23:39:40 -0500

    Linked List

--- Contest/linked/linked.cpp
+++ Contest/linked/linked.cpp
@@ -25,18 +25,20 @@ char _;
 
 using namespace std;
 
-ll N, kust = 2, pos = 1, front = 0, back = 0;
-ull cust[1000001];//[0,0] bit = Neg or not Next  [1,41] Bits Number [42,61]
+ull N, kust = 2, pos = 1, front = 0, back = 0;
+ull cust[1000001];//[0,0] bit = Neg or not Next  [1,43] Bits Number [44,64]
 
 int main() {
 	scan(N);
 	char c;
-	ll a;
+	ull a;
+	ull krust;
 	ll crust;
 	bool neg = false;
-	for (int i = 2; i < 999999; i++) {
+	for (int i = 2; i < 1000001; i++) {
 		cust[i] = i + 1;
 	}
+	cust[0] = 1 << 44ull;
 	for (int i = 0; i < N; i++) {
 		c = getchar();
 		if (c == '+') {
@@ -44,49 +46,53 @@ int main() {
 			scanf("%lld", &crust);
 			if (crust < 0) {
 				neg = true;
-				crust = -crust;
+				krust = -crust;
 			}
+			else
+				krust = crust;
 			a = kust;
 			kust = cust[kust];
-			cust[a] = crust << 1ll | neg | ((pos^back) << 42ll);
-			cust[pos] ^= (back << 42ll) ^ (a << 42ll);
-			cust[back] ^= (pos << 42ll) ^ (a << 42ll);
+			cust[a] = krust << 1ull | neg | ((pos^back) << 44ull);
+			cust[pos] ^= (back << 44ull) ^ (a << 44ull);
+			cust[back] ^= (pos << 44ull) ^ (a << 44ull);
 			front = pos;
 			pos = a;
 		}
 		else if (c == '<') {
 			front = pos;
 			pos = back;
-			back = (cust[pos] >> 42ll) ^ front;
+			back = (cust[pos] >> 44ull) ^ front;
 		}
 		else if (c == '>') {
 			back = pos;
 			pos = front;
-			front = (cust[pos] >> 42ll) ^ back;
+			front = (cust[pos] >> 44ull) ^ back;
 		}
 		else if (c == '=') {
 			neg = false;
 			scanf("%d", &crust);
 			if (crust < 0) {
 				neg = true;
-				crust = -crust;
+				krust = -crust;
 			}
-			cust[pos] = neg | crust << 1ll | ((cust[pos] >> 42ll) << 42ll);
+			else
+				krust = crust;
+			cust[pos] = neg | krust << 1ull | ((cust[pos] >> 44ull) << 44ull);
 		}
 		else if (c == '-') {
-			cust[front] ^= (pos << 42ll) ^ (back << 42ll);
-			cust[back] ^= (pos << 42ll) ^ (front << 42ll);
+			cust[front] ^= (pos << 44ull) ^ (back << 44ull);
+			cust[back] ^= (pos << 44ull) ^ (front << 44ull);
 			cust[pos] = kust;
 			kust = pos;
 			pos = front;
-			front = (cust[pos] >> 42ll) ^ back;
+			front = (cust[pos] >> 44ull) ^ back;
 		}
 		else if (c == '!') {
 			if (cust[pos] & 1) {
-				printf("-%lld\n", ((cust[pos] & ((1ll << 42ll) - 1ll))) >> 1ll);
+				printf("-%llu\n", (cust[pos]>>1ull) & 8796093022207ull);
 			}
 			else {
-				printf("%lld\n", ((cust[pos] & ((1ll << 42ll) - 1ll))) >> 1ll);
+				printf("%llu\n", (cust[pos]>>1ull) & 8796093022207ull);
 			}
 		}
 		getchar();
